920e26084ec1de69adadbda50eb846bcdc9d0598
Lisa Reinhardt <reinhal@gmail.com>
2019-04-13 19:34:21 -0400

    added router

---
+++ imageRoutes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var imageController = require('./imageController');
+var upload = require('./cloudinaryUploads/multer');
+
+var router = express.Router();
+
+router.post('/addImage', upload.any(), imageController.createApp);
+
+module.exports = router;
